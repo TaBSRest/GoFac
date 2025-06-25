a4785d2781ce48a9b1fc2c645075742b3fb92a75
Isaac Chun <inha.chun22@gmail.com>
2025-06-25 11:37:26 -0400

    feat(general): #42 Delete Unwrap Method

--- internal/TaBSError/TaBSError.go
+++ internal/TaBSError/TaBSError.go
@@ -112,10 +112,3 @@ func getCallerLocation(skipFrames int) string {
 func (e *TaBSError) GetMessage() string {
 	return e.message
 }
-
-func (e *TaBSError) Unwrap() error {
-	if len(e.children) == 0 {
-		return e.children[0] // Pick the first child as the primary cause
-	}
-	return New("The error contains multilpe errors. It cannot unwrap to one error.")
-}
